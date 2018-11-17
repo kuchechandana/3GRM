54e785a8a3d105a58616003a32a0252e0d9fae1c
kuchechandana <35056039+kuchechandana@users.noreply.github.com>
2018-11-17 18:09:06 +0530

    Python program to compare two sets

---
+++ Programming/Python/set7.py
@@ -0,0 +1,6 @@
+DaysA = set(["Mon","Tue","Wed"])
+DaysB = set(["Mon","Tue","Wed","Thu","Fri","Sat","Sun"])
+SubsetRes = DaysA <= DaysB
+SupersetRes = DaysB >= DaysA
+print(SubsetRes)
+print(SupersetRes)
